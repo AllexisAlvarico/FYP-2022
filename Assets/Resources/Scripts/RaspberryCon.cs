d35688141e5cd376ec95e8d7bc4a9f269cf4e285
Allexis Alvarico <44900809+AllexisAlvarico@users.noreply.github.com>
2022-03-22 18:39:16 +0000

    Added a tcp listener

---
+++ Assets/Resources/Scripts/RaspberryCon.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System;
+
+public class RaspberryCon : MonoBehaviour
+{
+
+    public string host;
+    public int port;
+
+    internal bool socket_ready = false;
+    internal string input_buffer = "";
+    TcpClient tcp_client;
+    NetworkStream net_stream;
+
+    StreamWriter socket_writer;
+    StreamReader socket_reader;
+
+
+    private void Start()
+    {
+        setupSocket();
+    }
+
+
+    void setupSocket()
+    {
+        try
+        {
+            tcp_client = new TcpClient();
+
+            IPAddress iPAddress = Dns.GetHostEntry(host).AddressList[0];
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 50000);
+            tcp_client.Connect(iPEndPoint);
+
+            net_stream = tcp_client.GetStream();
+            socket_writer = new StreamWriter(net_stream);
+            socket_reader = new StreamReader(net_stream);
+
+            socket_ready = true;
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket Error " + e);
+        }
+    }
+
+    public void writeSocket(string line)
+    {
+        if (!socket_ready)
+            return;
+
+        line = line + "\r\n";
+        socket_writer.Write(line);
+        socket_writer.Flush();
+    }
+
+    public String readSocket()
+    {
+        if (!socket_ready)
+            return "";
+
+        if (net_stream.DataAvailable)
+            return socket_reader.ReadLine().ToString();
+
+        return "";
+    }
+
+    public void closeSocket()
+    {
+        if (!socket_ready)
+            return;
+
+        socket_writer.Close();
+        socket_reader.Close();
+        tcp_client.Close();
+        socket_ready = false;
+    }
+}
